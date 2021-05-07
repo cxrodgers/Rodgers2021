7bfc5c04b6fc9847a25bf952a04772629a777df4
Chris Rodgers <xrodgers@gmail.com>
2021-05-07 15:04:15 -0400

    mplconfigdir and skip_start

--- runall.py
+++ runall.py
@@ -4,6 +4,9 @@
 conda create --name Rodgers2021 --file requirements.txt
 conda activate Rodgers2021
 pip install -r pip_requirements.txt
+
+Then set this config var to ensure agg is always used
+conda env config vars set MPLCONFIGDIR=/home/jack/dev/Rodgers2021/mpl_config_dir
 """
 
 import subprocess
@@ -15,6 +18,8 @@ this_file_dir = os.path.dirname(this_filename)
 print("The name of this file is {}".format(this_filename))
 print("Starting in {}".format(this_file_dir))
 
+start_at = '01_patterns', 'main2b.py'
+skip_start_flag = True
 
 ## Get dirnames to run
 dirnames = [
@@ -31,6 +36,10 @@ dirnames = [
 
 try:
     for dirname in dirnames:
+        if skip_start_flag:
+            if dirname != start_at[0]:
+                continue
+        
         # CWD
         print("\tswitching to {}".format(dirname))
         os.chdir(dirname)
@@ -41,10 +50,18 @@ try:
             ['\t- ' + line for line in pyfilenames])))
 
         for pyfilename in pyfilenames:
+            if skip_start_flag:
+                if pyfilename != start_at[1]:
+                    continue            
+                else:
+                    skip_start_flag = False
+                    
             # Run
             print("\t\trunning {} in {}".format(pyfilename, os.getcwd()))
             proc_output = subprocess.check_output([
-                'python', pyfilename,
+                'python', 
+                '-W', 'ignore:Matplotlib is currently using agg:UserWarning::', 
+                pyfilename,
                 ])
             
             # Decode
@@ -55,10 +72,7 @@ try:
             print('\n'.join([
                 '\t\t- {}'.format(line) for line in proc_output.split('\n')
                 ]))
-            
-            if pyfilename.startswith('main1a'):
-                
-                1/0
+        1/0
 
 except subprocess.CalledProcessError:
     print("Encountered CalledProcessError, aborting")
@@ -67,3 +81,5 @@ finally:
     print("finally")
     print("return to {}".format(this_file_dir))
     os.chdir(this_file_dir)
+
+    os.environ['MPLCONFIGDIR'] = ''    
